99c3a3d33a3fb0ff2dbce0d43d5b587824555909
gui-yupeng <1413137503@qq.com>
2021-04-24 18:19:52 +0800

    check中间件报错

--- middleware/check.js
+++ middleware/check.js
@@ -1,16 +1,19 @@
-const crypto = require('crypto');
 const mongo = require('../mongodb.js');
 const dbMsg=require('../../dbMsg.json');
-const jwt = require('jsonwebtoken');
-const { nextTick } = require('process');
-const { createContext } = require('vm');
 
-module.exports=async(ctx,next)=>{
-
-  if(ctx.path==='/admin'){
-
-    await next();
+module.exports = async (ctx, next) => {
+  let path=ctx.path;
+  //只有登陆(/admin/login)不需要token，其他api都需要
+  if(path==='/admin'&&path==='/admin/details'&&path==='/admin/details/appointList'){
+    let token=ctx.request.headers["x-api-token"];
+    let adminCollection=await mongo(dbMsg.col_admin);
+    let admin= await adminCollection.findOne({"token":token});
+    if(!admin){
+      //不执行下游next()
+      ctx.body='noToken';
+      return;
+    }else{
+      await next();
+    }
   }
-  await next();
-
 }
