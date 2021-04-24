c55d37f2f87b3e6cbbd3ce0e27cbab4c88b4b9f3
gui-yupeng <1413137503@qq.com>
2021-04-24 13:29:05 +0800

    admin可登陆，但登陆逻辑一切从简，后续有时间完善

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,23 +1,32 @@
 const mongo=require('../../mongodb.js');
 const dbMsg=require('../../dbMsg.json');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto-js');
 
 exports.route={
 
-    //身份认证，管理员是否在数据库中
+    //身份认证，只有index(获取当前全部讲座信息)不需要token，其余api均需要token
     async post() {
         let adminData=this.params;
         let cardNum=adminData.cardNum;
-        let password=adminData.password;
+        let pwd=adminData.password;
+        //MD5加密
+        password=crypto.MD5(pwd).toString();
+        //token
+        let tokenObj={Num:cardNum};
+        let token=jwt.sign(tokenObj, 'ThanksForLLY');
         let adminCollection=await mongo(dbMsg.col_admin);
         let admin= await adminCollection.findOne({"cardNum":cardNum});
-        if(admin){
-            if(password!=admin.password){
-                this.throw(401,'密码错误！');
-            }
-        }else{
-            this.throw(401,'您没有管理员权限！')
+        if(!admin){
+            //不是管理员
+            return {"result":"noCardNum"};
         }
-        return 'OK';
+        if(admin.password!=password){
+            //密码不对
+            return {"result":"wrongPassword"};
+        }
+        return {"result":"ok","token":token};
+        
     }
 
 };
